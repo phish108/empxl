d492321342b044f3fed90c3a746909044e3cf876
Christian Glahn <cpglahn@gmail.com>
2024-04-19 00:09:16 +0200

    MS uses // as comments

--- bin/compile.sh
+++ bin/compile.sh
@@ -4,5 +4,5 @@ mkdir -p compile
 
 for FILE in $(ls src/de)
 do 
-    cat src/de/$FILE | sed 's/\s*#.*$//g' | tr -d "[:space:]" > compile/$FILE
+    cat src/de/$FILE | sed 's/\s*\/\/.*$//g' | tr -d "[:space:]" > compile/$FILE
 done
