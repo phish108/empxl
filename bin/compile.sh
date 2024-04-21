ef5f9920d1d4cfcd117c01b9e18464aae92abe9a
Christian Glahn <cpglahn@gmail.com>
2024-04-22 00:55:27 +0200

    new compile script

--- bin/compile.sh
+++ bin/compile.sh
@@ -2,7 +2,12 @@
 
 mkdir -p compile
 
+echo "" > empxl.afe
 for FILE in $(ls src/de)
 do 
     cat src/de/$FILE | sed 's/\s*\/\/.*$//g' | tr -d "[:space:]" > compile/$FILE
+
+    # compile into a AFE module
+    cat compile/$FILE >> empxl.afe
+    echo "" >> empxl.afe
 done
